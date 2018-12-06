be6d6cab47a256d3d471af17ad8e3f8656b11970
Noah Haasis <haasis_noah@yahoo.de>
2018-12-06 09:35:54 +0100

    Initial commit

---
+++ Regex.hs
@@ -0,0 +1,47 @@
+module Regex (Language (..), match, matchLanguage) where
+
+import Data.List.Split
+
+data Language
+  = Singleton Char
+  | Alt Language Language
+  | Cat Language Language
+  | Rep Language
+  | Eps
+  | Empty
+  deriving (Show)
+
+isAcceptingLang :: Language -> Bool
+isAcceptingLang Empty         = False
+isAcceptingLang (Singleton _) = False
+isAcceptingLang Eps           = True
+isAcceptingLang (Rep _)       = True
+isAcceptingLang (Alt l1 l2)   = isAcceptingLang l1 || isAcceptingLang l2
+isAcceptingLang (Cat l1 l2)  = isAcceptingLang l1 && isAcceptingLang l2
+
+derive :: Char -> Language -> Language
+derive _ Empty         = Empty
+derive _ Eps           = Empty
+derive c (Singleton d) = if c == d then Eps else Empty
+derive c (Cat l1 l2)  =
+  if isAcceptingLang l1 then Alt (Cat  (derive c l1) l2) (derive c l2)
+  else Cat (derive c l1) l2
+derive c (Alt l1 l2)   = Alt (derive c l1) (derive c l2)
+derive c (Rep l)       = Cat (derive c l) (Rep l)
+
+matchLanguage :: Language -> String -> Bool
+matchLanguage l ""     =  isAcceptingLang l
+matchLanguage l (x:xs) = matchLanguage (derive x l) xs
+
+
+strToLanguage :: String -> Language
+strToLanguage p = case splitOn "|" p of
+  [x] -> conjunct $ toSingletonList p
+  xs  -> disjunct $ map strToLanguage xs
+  where
+    toSingletonList = map Singleton
+    conjunct = (foldr Cat Eps)
+    disjunct = (foldr Alt Empty)
+
+match :: String -> String -> Bool
+match p s = matchLanguage (strToLanguage p) s
---
+++ Spec.hs
@@ -0,0 +1,55 @@
+import Test.Hspec
+
+import Regex
+
+main :: IO ()
+main = hspec $ do
+  describe "matching string with language" $ do
+    it "matches single letter" $ do
+      matchLanguage (Singleton 'c') "c" `shouldBe` True
+
+    it "matches empty word" $ do
+      matchLanguage Eps "" `shouldBe` True
+
+    it "doesn't matchLanguage empty language" $ do
+      matchLanguage Empty "" `shouldBe` False
+
+    it "matches multiple chars" $ do
+      matchLanguage (Cat (Singleton 'a') (Singleton 'b')) "ab" `shouldBe` True
+
+    it "matches the empty string for repetition" $ do
+      matchLanguage (Rep (Singleton 'd')) "" `shouldBe` True
+
+    it "matches multiple chars for repetition" $ do
+      matchLanguage (Rep (Singleton 'd')) "dddd" `shouldBe` True
+
+    it "matches numbers" $ do
+      let digits = map Singleton "0123456789"
+          digit = foldr Alt Empty digits
+          number = Cat digit (Rep digit)
+      matchLanguage number "12981" `shouldBe` True
+
+  describe "matching string with regular expression" $ do
+    it "pattern \"a\" matches pattern \"a\"" $ do
+      match "a" "a" `shouldBe` True
+
+    it "pattern \"a\" doesn't match \"b\"" $ do
+      match "a" "b" `shouldBe` False
+
+    it "pattern \"c\" matches \"c\"" $ do
+      match "c" "c" `shouldBe` True
+
+    it "pattern \"c\" doesn't match \"d\"" $ do
+      match "c" "d" `shouldBe` False
+
+    it "pattern \"abc\" matches \"abc\"" $ do
+      match "abc" "abc" `shouldBe` True
+
+    it "pattern \"dog|cat\" matches \"dog\"" $ do
+      match "dog|cat" "dog" `shouldBe` True
+
+    it "pattern \"dog|cat\" matches \"cat\"" $ do
+      match "dog|cat" "cat" `shouldBe` True
+
+    it "pattern \"dog|cat\" doesn't match \"ape\"" $ do
+      match "dog|cat" "ape" `shouldBe` False

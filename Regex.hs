384a9064643d4560edf604f6149abf31e407730b
Noah Haasis <haasis_noah@yahoo.de>
2018-12-15 08:17:37 +0100

    Add TODOS

--- Regex.hs
+++ Regex.hs
@@ -34,22 +34,24 @@ derive c (Alt l1 l2)   = Alt (derive c l1) (derive c l2)
 derive c (Rep l)       = Cat (derive c l) (Rep l)
 
 matchLanguage :: Language -> String -> Bool
-matchLanguage l ""     =  isAcceptingLang l
+matchLanguage l ""     = isAcceptingLang l
 matchLanguage l (x:xs) = matchLanguage (derive x l) xs
 
 {-======================= Parser =======================-}
 {- Grammar:
 
-DISJUNCTION  -> EXPR1 {'|' EXPR1}
-EXPR1        -> EXPR2 EXPR1 | ε
-EXPR2        -> REP | EXPR3
-REP          -> EXPR3 '*'
-EXPR3        -> RANGE | DOT | SINGLETON | '(' EXPR ')'
-DOT          -> '.'
-RANGE        -> '[' {SINGLETON} ']'
-SINGLETON    -> 'a' | 'b' | 'c' | 'd' ...
+DISJUNCTION    -> EXPR1 {'|' EXPR1}
+EXPR1          -> EXPR2 EXPR1 | ε
+REPORSINGLETON -> REP | EXPR3
+REP            -> EXPR3 '*'
+EXPR3          -> RANGE | DOT | SINGLETON | '(' EXPR ')'
+DOT            -> '.'
+RANGE          -> '[' {SINGLETON} ']'
+SINGLETON      -> 'a' | 'b' | 'c' | 'd' ...
 
+TODO: Implement the BNF
 TODO: Handle escaped characters
+TODO: There actually are unacceptable regular expressions: "|*" should not be acceptible
 
 -}
 
